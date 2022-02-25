389937e489af1ec3ef2b37e1a60ef71c0e567262
tofusoup429 <tofusoup429@gmail.com>
2022-02-25 18:03:13 +0900

    Update useCountdown.ts

--- useCountdown.ts
+++ useCountdown.ts
@@ -1,9 +1,16 @@
 import {useState, useEffect} from 'react'
 export  const useCountdown = (startFrom:number, startCountDown:boolean=false, timeoutFunction:any, stopCountingDown:boolean) =>{
+    /*
+        startFrom: number that the countdown starts from. 
+        startCountDown: turnning this from false to true triggers countdown. 
+        timeoutFunction: a function that will be triggered when countdown reaches 0
+        stopCountingDown: if it turns true, countdown stops
+    */
    const [counter, handleCounter] = useState(startFrom);   
    useEffect(()=>{
     if(startCountDown){
         const countdown = setInterval(()=>handleCounter(old=>old-1), 1000);
+        //there are two ways to stop countdown. One is letting it hits 0 and the other is you pass stopCountingDown true.  
         if(stopCountingDown){
             clearInterval(countdown);
             handleCounter(startFrom);
@@ -18,11 +25,10 @@ export  const useCountdown = (startFrom:number, startCountDown:boolean=false, ti
 
    useEffect(()=>{
     if(counter===0){
+        //when counter reaches 0, timeoutFunction is executed.
         timeoutFunction();
         handleCounter(startFrom)
     }
    }, [counter])
-   
-   
    return {counter}
 } 
